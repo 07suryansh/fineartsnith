3950c420f273817c5bd4fc727a534f6d03b7c33e
Suryansh Singh Bisen <07suryansh@gmail.com>
2023-12-26 21:11:11 +0530

    created navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import './navbar.css'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+        <div className="navbar">
+            <div className="left-navbar">
+                Finearts NITH
+            </div>
+            <div className="right-navbar">
+                    <a href='/'>Home</a>
+                    <a href='/'>Gallery</a>
+                    <a href='/'>Team</a>
+                    <a href='/'>Contact</a>
+            </div>
+        </div>
+    </>
   )
 }
