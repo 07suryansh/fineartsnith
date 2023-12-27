9b85fed28503bf275f05602fda7df1f782a766f0
Suryansh Singh Bisen <07suryansh@gmail.com>
2023-12-27 10:11:28 +0530

    given shadow to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import './navbar.css'
+import './navbar.css';
 
 export default function Navbar() {
+  
+
   return (
     <>
-        <div className="navbar">
-            <div className="left-navbar">
-                Finearts NITH
-            </div>
-            <div className="right-navbar">
-                    <a href='/'>Home</a>
-                    <a href='/gallery'>Gallery</a>
-                    <a href='/team'>Team</a>
-                    <a href='/contact'>Contact</a>
-            </div>
+      <div className={"navbar"}>
+        <div className="left-navbar">Finearts NITH</div>
+        <div className="right-navbar">
+          <a href='/'>Home</a>
+          <a href='/gallery'>Gallery</a>
+          <a href='/team'>Team</a>
+          <a href='/contact'>Contact</a>
         </div>
+      </div>
     </>
-  )
+  );
 }
