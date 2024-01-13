e3bb94640a9ba5ea7ff9a3faa1848a2b3f6b7ef1
Suryansh Singh Bisen <07suryansh@gmail.com>
2024-01-13 21:20:54 +0530

    adjusted margin and padding

--- src/components/Home.js
+++ src/components/Home.js
@@ -41,15 +41,15 @@ export default function Home() {
     <>
       <div className="home">
         <h1>Welcome</h1>
-        <p>Step into the vibrant world of our Fine Arts Club at <b>National Institute of Technology Hamirpur</b>, where creativity flourishes and artistic expression takes center stage. Within our gallery, discover the incredible talents of our student artists who weave visual stories with passion and flair. From vivid canvases that burst with youthful energy to sculptures that embody the innovative spirit of our community, our collection reflects the diverse and dynamic artistry that thrives within our college walls. Join us in celebrating the extraordinary talents that shape our artistic landscape. Our Fine Arts Club is a collaborative space where students, each a brushstroke in the canvas of our community, explore their creative boundaries and showcase the beauty that resides in every piece. Immerse yourself in the world of colors, shapes, and emotions as we invite you to be a part of our creative journey. Welcome to the heart of artistic expression at <b>National Institute of Technology Hamirpur</b>, where the gallery echoes with the enthusiasm of emerging artists, and every masterpiece tells a story unique to our college community.</p>
         <div className="display-image">
-          <img src="" alt="" />
           {data.map((item) =>
               item.keyword === "home" ? (
                 <img src={item.imageUrl} alt="" />
               ) : null
             )}
         </div>
+        <p>Step into the vibrant world of our Fine Arts Club at <b>National Institute of Technology Hamirpur</b>, where creativity flourishes and artistic expression takes center stage. Within our gallery, discover the incredible talents of our student artists who weave visual stories with passion and flair. From vivid canvases that burst with youthful energy to sculptures that embody the innovative spirit of our community, our collection reflects the diverse and dynamic artistry that thrives within our college walls. Join us in celebrating the extraordinary talents that shape our artistic landscape. Our Fine Arts Club is a collaborative space where students, each a brushstroke in the canvas of our community, explore their creative boundaries and showcase the beauty that resides in every piece. Immerse yourself in the world of colors, shapes, and emotions as we invite you to be a part of our creative journey. Welcome to the heart of artistic expression at <b>National Institute of Technology Hamirpur</b>, where the gallery echoes with the enthusiasm of emerging artists, and every masterpiece tells a story unique to our college community.</p>
+        
       </div>
     </>
   )
