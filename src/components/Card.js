e5290c045c6f091b6963b02e6ad7c355df316aff
Suryansh Singh Bisen <07suryansh@gmail.com>
2024-01-14 16:43:53 +0530

    done image non draggable

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ export default function Card(props) {
   return (
     <>
         <div className="card">
-            <img src={props.imgurl} alt="" className='card-img'/>
+            <img src={props.imgurl} alt="" className='card-img' draggable="false"/>
             <div className="name">{props.name}</div>
         </div>
     </>
